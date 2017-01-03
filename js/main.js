14ea4bb874631675cb1c80636465363bf9d92380
Viktoriia Natashkina <natashkinavika@gmail.com>
2017-01-03 22:21:43 +0200

    fixed owl-carousel

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){ 
 $('.owl-carousel').owlCarousel({ 
-items:4, 
+items:1, 
 lazyLoad:true, 
 loop:true, 
 margin:10, 
-nav: true 
 }); 
 });
 
 $(document).ready(function(){ 
 $('.owl-carousel-1').owlCarousel({ 
-items:1, 
+items:4, 
 lazyLoad:true, 
 loop:true, 
 margin:10, 
-nav: true 
 }); 
 });
-
+ 
